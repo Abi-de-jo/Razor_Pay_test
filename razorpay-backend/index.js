fc3abe8f85a26f60e6bd73ed1703b04ce54d2be0
Abi-de-jo <abisheikabisheik102@gmail.com>
2025-05-23 00:42:12 +0530

    export

--- razorpay-backend/index.js
+++ razorpay-backend/index.js
@@ -29,4 +29,6 @@ app.post("/create-order", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+// app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+
+export default app;
